ce375f232310b53adf8fe4f24cef5225610ae921
Flávio Augusto Oliveira Costa <faugusto@Flavios-MacBook-Pro.local>
2022-10-25 18:21:59 -0300

    Implementado Soma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma();
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.Write("Por favor digite os valores: ");
+            double numero1 = float.Parse(Console.ReadLine());
+            double numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("O resultado da soma de {0} com {1} é {2}", numero1, numero2, numero1+numero2);
+        }
+        static void Subtracao()
+        {
+
+        }
+        static void Multiplicacao()
+        {
+
+        }
+        static void Divisao()
+        {
+
+        }
+    }
+    
+}
