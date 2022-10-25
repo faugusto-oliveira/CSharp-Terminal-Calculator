583a1e43985186170faf74efceeb4693d9809963
Flávio Augusto Oliveira Costa <faugusto@Flavios-MacBook-Pro.local>
2022-10-25 18:26:29 -0300

    Implementado subtração, multiplicação e Divisão

--- Program.cs
+++ Program.cs
@@ -18,15 +18,27 @@ namespace Calculator
         }
         static void Subtracao()
         {
-
+            Console.Clear();
+            Console.Write("Por favor digite os valores: ");
+            double numero1 = float.Parse(Console.ReadLine());
+            double numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("O resultado da subtração de {0} com {1} é {2}", numero1, numero2, numero1-numero2);
         }
         static void Multiplicacao()
         {
-
+            Console.Clear();
+            Console.Write("Por favor digite os valores: ");
+            double numero1 = float.Parse(Console.ReadLine());
+            double numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da multiplicação de {numero1} com {numero2} é {numero1*numero2}");
         }
         static void Divisao()
         {
-
+            Console.Clear();
+            Console.Write("Por favor digite os valores: ");
+            double numero1 = float.Parse(Console.ReadLine());
+            double numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"O resultado da divisão de {numero1} com {numero2} é {numero1/numero2}");
         }
     }
     
