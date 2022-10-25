636e0ce517bddbced2ea3ac51d088878f4f38868
Flávio Augusto Oliveira Costa <faugusto@Flavios-MacBook-Pro.local>
2022-10-25 18:47:01 -0300

    Adicionado Menu e callback

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Seja bem vindo! O que deseja fazer? ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração ");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+            short modificador = short.Parse(Console.ReadLine());
+            switch (modificador)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+                default: Menu(); break;
+            }
         }
         static void Soma()
         {
@@ -15,6 +34,7 @@ namespace Calculator
             double numero1 = float.Parse(Console.ReadLine());
             double numero2 = float.Parse(Console.ReadLine());
             Console.WriteLine("O resultado da soma de {0} com {1} é {2}", numero1, numero2, numero1+numero2);
+            Menu();
         }
         static void Subtracao()
         {
@@ -23,6 +43,7 @@ namespace Calculator
             double numero1 = float.Parse(Console.ReadLine());
             double numero2 = float.Parse(Console.ReadLine());
             Console.WriteLine("O resultado da subtração de {0} com {1} é {2}", numero1, numero2, numero1-numero2);
+            Menu();
         }
         static void Multiplicacao()
         {
@@ -31,15 +52,25 @@ namespace Calculator
             double numero1 = float.Parse(Console.ReadLine());
             double numero2 = float.Parse(Console.ReadLine());
             Console.WriteLine($"O resultado da multiplicação de {numero1} com {numero2} é {numero1*numero2}");
+            Menu();
         }
         static void Divisao()
         {
-            Console.Clear();
-            Console.Write("Por favor digite os valores: ");
-            double numero1 = float.Parse(Console.ReadLine());
-            double numero2 = float.Parse(Console.ReadLine());
-            Console.WriteLine($"O resultado da divisão de {numero1} com {numero2} é {numero1/numero2}");
+            try
+            {
+                Console.Clear();
+                Console.Write("Por favor digite os valores: ");
+                double numero1 = float.Parse(Console.ReadLine());
+                double numero2 = float.Parse(Console.ReadLine());
+                Console.WriteLine($"O resultado da divisão de {numero1} com {numero2} é {numero1/numero2}");
+                Menu();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Divisão por zero");
+                Menu();
+            }
+            
         }
     }
-    
 }
